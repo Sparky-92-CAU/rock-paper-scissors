ad39818e2ca420da79cdc4e650a9f54c8beae336
Sparky-92-CAU <urbonc@gmail.com>
2022-04-01 17:42:03 -0400

    Create computerPlay() Function
    created function which randomly returns rock, paper, or scissors.

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 
-console.log("hello world")
+
+function computerPlay(){
+    
+    
+   let move = Math.floor(Math.random() * 3) + 1;
+
+   if(move==3){
+       return "rock";
+   }
+   else if(move==2){
+       return "paper";
+   }
+   else{
+       return "scissors";
+   }
+}
+
 
 
 
