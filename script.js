f4432174a3f3ddbc9d8c5bfb82415a0540b72ad3
Sparky-92-CAU <urbonc@gmail.com>
2022-04-01 18:19:07 -0400

    Create playRound() Function
    play round takes a user selection and the computer selection, compares the two, and returns the result.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+//randomly returns rock, paper, or scissors
 function computerPlay(){
     
     
@@ -16,6 +16,65 @@ function computerPlay(){
    }
 }
 
+/*Takes in a player selection and computer selection and plays a single round of r,p,s
+*/
+
+function playRound(playerSelection,compSelection){
+    //make user input case insensitive
+    let playerMove = playerSelection.toLowerCase();
+    //store result message into vars
+    let winMsg = `You Win! ${playerSelection} beats ${compSelection}`;
+    let loseMsg = `You Lose! ${compSelection} beats ${playerSelection}`;
+
+    //structure to compare moves
+    if(playerMove == 'rock'){
+       if(compSelection == 'rock'){
+           return 'Draw!';
+       }
+       else if(compSelection == 'paper'){
+           return loseMsg;
+       }
+       else{
+           return winMsg;
+       }
+    }
+    else if(playerMove == 'paper'){
+        if(compSelection == 'paper'){
+            return 'Draw!';
+        }
+        else if(compSelection == 'rock'){
+            return winMsg;
+        }
+        else{
+            return loseMsg;
+        }
+
+    }
+    else if(playerMove == 'scissors'){
+
+        if(compSelection == 'scissors'){
+            return 'Draw!';
+        }
+        else if(compSelection == 'rock'){
+            return loseMsg;
+        }
+        else{
+            return winMsg;
+        }
+
+    }
+    else{
+        return "You did not enter an appropriate move!";
+    }
+
+
+
+}
+
+const playerSelection = "paper";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
+console.log(computerSelection);
 
 
 
